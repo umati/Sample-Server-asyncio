8c5ddbbc886bdbc64e093eb52a51606ff17bc251
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2020-07-16 19:18:15 +0200

    feat(Server): Initial set of files

---
+++ src/generate_export_namespace.py
@@ -0,0 +1,86 @@
+# Imports
+from asyncua import ua, uamethod, Server
+import os, asyncio
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+NAME = "VDMA-OPC-Surface-Technology-Initiative-CS"
+XML_FILENAME = os.path.join(NAME + ".xml")
+NAMESPACE = "http://vdma-opc-st-initiative-cs/ua"
+
+async def main():
+    # Setup Namespace
+    server = Server()
+    await server.init()
+    ns_idx = await server.register_namespace(NAMESPACE)
+
+    base_object_type = server.get_node(ua.ObjectIds.BaseObjectType)
+
+    '''
+    Behälter:
+    '''
+    vessel_object_type = await base_object_type.add_object_type(ns_idx, "VesselObjectType")
+
+    vessel_name = await vessel_object_type.add_variable(ns_idx, "Name", "BehälterName", ua.VariantType.String)
+    await vessel_name.set_writable(False) # True = writable
+    await vessel_name.set_modelling_rule(True) # True = mandatory
+
+    vessel_location = await vessel_object_type.add_variable(ns_idx, "Location", "BehälterLocation", ua.VariantType.String)
+    await vessel_location.set_writable(False) # True = writable
+    await vessel_location.set_modelling_rule(True) # True = mandatory
+
+    vessel_state = await vessel_object_type.add_variable(ns_idx, "State", 0, ua.VariantType.Int16)
+    await vessel_state.set_writable(False) # True = writable
+    await vessel_state.set_modelling_rule(True) # True = mandatory
+
+    vessel_level = await vessel_object_type.add_variable(ns_idx, "Level", 0.0, ua.VariantType.Float)
+    await vessel_level.set_writable(False) # True = writable
+    await vessel_level.set_modelling_rule(True) # True = mandatory
+
+    '''
+    Rührwerk:
+    '''
+    agitator_object_type = await base_object_type.add_object_type(ns_idx, "AgitatorObjectType")
+
+    agitator_name = await agitator_object_type.add_variable(ns_idx, "Name", "RührwerkName", ua.VariantType.String)
+    await agitator_name.set_writable(False) # True = writable
+    await agitator_name.set_modelling_rule(True) # True = mandatory
+
+    agitator_location = await agitator_object_type.add_variable(ns_idx, "Location", "RührwerkLocation", ua.VariantType.String)
+    await agitator_location.set_writable(False) # True = writable
+    await agitator_location.set_modelling_rule(True) # True = mandatory
+
+    agitator_speed = await agitator_object_type.add_variable(ns_idx, "Speed", 0.0, ua.VariantType.Float)
+    await agitator_speed.set_writable(False) # True = writable
+    await agitator_speed.set_modelling_rule(True) # True = mandatory
+
+    '''
+    Ventil:
+    '''
+    valve_object_type = await base_object_type.add_object_type(ns_idx, "ValveObjectType")
+
+    valve_name = await valve_object_type.add_variable(ns_idx, "Name", "VentilName", ua.VariantType.String)
+    await valve_name.set_writable(False) # True = writable
+    await valve_name.set_modelling_rule(True) # True = mandatory
+
+    valve_type = await valve_object_type.add_variable(ns_idx, "Type", "VentilTyp", ua.VariantType.String)
+    await valve_type.set_writable(False) # True = writable
+    await valve_type.set_modelling_rule(True) # True = mandatory
+
+    valve_state = await valve_object_type.add_variable(ns_idx, "State", 0, ua.VariantType.Int16)
+    await valve_state.set_writable(False) # True = writable
+    await valve_state.set_modelling_rule(True) # True = mandatory
+
+
+    # Export Namespace as xml
+    try:
+        os.remove(os.path.join(BASE_DIR, "nodeset", XML_FILENAME))
+    except FileNotFoundError:
+        pass
+    except Exception as e:
+        print(e)
+
+    await server.export_xml_by_ns(os.path.join(BASE_DIR, "nodeset", XML_FILENAME), namespaces=NAMESPACE)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ src/server.py
@@ -0,0 +1,54 @@
+# Imports
+import os, asyncio
+from asyncua import Server, ua, uamethod
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+NAME = "VDMA-OPC-Surface-Technology-Initiative-CS"
+XML_FILENAME = os.path.join(NAME + ".xml")
+
+NAMESPACE = "http://vdma-opc-st-initiative-cs/ua/Prototyp1"
+NAMESPACE_CS = "http://vdma-opc-st-initiative-cs/ua"
+
+async def main():
+    # Serversetup
+    server = Server()
+    await server.init()
+    server.set_endpoint("opc.tcp://127.0.0.1:4840")
+    idx = await server.register_namespace(NAMESPACE)
+
+    # Import nodes.xml
+    try:
+        await server.import_xml(os.path.join(BASE_DIR, "nodeset", XML_FILENAME))
+    except FileNotFoundError as e:
+        print(f"FileNotFoundError: {e}")
+    except Exception as e:
+        print(e)
+
+    objects = server.get_objects_node()
+    idx_cs = await server.get_namespace_index(NAMESPACE_CS)
+
+    fg1 = await objects.add_object(idx, "FG01 - Ansatz", objecttype=ua.ObjectIds.BaseObjectType)
+    fg2 = await objects.add_object(idx, "FG02 - Versorgung", objecttype=ua.ObjectIds.BaseObjectType)
+
+    # instantiate nodes from nodeset
+    # my_vessel = await objects.add_object(idx, "MyVessel", "ns=3;i=1")
+    await fg1.add_object(idx, "Behälter", f"ns={idx_cs};i=1")
+    await fg2.add_object(idx, "Behälter", f"ns={idx_cs};i=1")
+
+    # my_agitator = await objects.add_object(idx, "MyAgitator", "ns=3;i=6")
+    await fg1.add_object(idx, "Rührwerk", f"ns={idx_cs};i=6")
+    await fg2.add_object(idx, "Rührwerk", f"ns={idx_cs};i=6")
+
+    # my_valve = await objects.add_object(idx, "MyValve", "ns=3;i=10")
+    await fg1.add_object(idx, "Valve", f"ns={idx_cs};i=10")
+    await fg2.add_object(idx, "Valve1", f"ns={idx_cs};i=10")
+    await fg2.add_object(idx, "Valve2", f"ns={idx_cs};i=10")
+
+    async with server:
+        while 1:
+            await asyncio.sleep(1)
+
+# Start Server
+if __name__ == "__main__":
+    asyncio.run(main())
