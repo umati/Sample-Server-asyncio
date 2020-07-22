422fa44cdb1ddd6fa3b36b5d27b27c41739e2888
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2020-07-22 14:09:46 +0200

    fix(Server): Update generate_export_namespace.py

--- src/generate_export_namespace.py
+++ src/generate_export_namespace.py
@@ -17,128 +17,75 @@ async def main():
     base_object_type = server.get_node(ua.ObjectIds.BaseObjectType)
 
     '''
-    Behälter Metadaten
-    '''
-    vessel_meta_object_type = await base_object_type.add_object_type(ns_idx, "VesselMetaObjectType")
-
-    # Metadaten
-    vessel_meta_object = await vessel_object_type.add_object(ns_idx, "Meta")
-    await vessel_meta_location.set_modelling_rule(True)
-
-    vessel_meta_serial = await vessel_meta_object_type.add_property(ns_idx, "SerialNumber", "", ua.VariantType.String)
-    await vessel_meta_serial.set_modelling_rule(True)
-
-    vessel_meta_volume = await vessel_meta_object_type.add_property(ns_idx, "Volume", "", ua.VariantType.String)
-    await vessel_meta_volume.set_modelling_rule(True)
-
-    '''
-    Behälter State
-    '''
-    vessel_state_object_type = await base_object_type.add_object_type(ns_idx, "VesselStateObjectType")
-
-    vessel_state_operationalstate = await vessel_state_object_type.add_variable(ns_idx, "OperationaState", 0, ua.VariantType.Int64) # Datentyp?
-    await vessel_state_operationalstate.set_modelling_rule(True) # Mandatory ?
-
-    vessel_state_operationalhours = await vessel_state_object_type.add_variable(ns_idx, "OperationaHours", 0, ua.VariantType.Int64) # Datentyp?
-    await vessel_state_operationalhours.set_modelling_rule(True) # Mandatory ?
-
-    vessel_state_poweronhours = await vessel_state_object_type.add_variable(ns_idx, "PowerOnHours", ua.Variant(datetime.datetime.utcnow(),ua.VariantType.DateTime), ua.VariantType.DateTime) # Datentyp? -> hh:mm:ss ?
-    await vessel_state_poweronhours.set_modelling_rule(True) # Mandatory ?
-
-    '''
-    Parameter (Soll/Ist)
-    '''
-    vessel_parameter_object_type = await base_object_type.add_object_type(ns_idx, "VesselParameterObjectType")
-
-    vessel_parameter_setpoint = await vessel_parameter_object_type.add_variable(ns_idx, "Sollwert", 0, ua.VariantType.Float)
-    await vessel_parameter_setpoint.set_modelling_rule(True) # Mandatory ?
-
-    vessel_parameter_act_val = await vessel_parameter_object_type.add_variable(ns_idx, "Istwert", 0, ua.VariantType.Float)
-    await vessel_parameter_act_val.set_modelling_rule(True) # Mandatory ?
-
-
-    '''
     Behälter:
     '''
     vessel_object_type = await base_object_type.add_object_type(ns_idx, "VesselObjectType")
 
-    #meta
-    # await instantiate(vessel_object_type, vessel_meta_object_type, idx=ns_idx ,instantiate_optional=True)
-    vessel_object_meta = await vessel_object_type.add_object(ns_idx, "Metadaten", objecttype=vessel_meta_object_type) # <--------------------- wird nicht richtig instanziert!
-    await vessel_object_meta.set_modelling_rule(True)
-    #state
-    vessel_object_state = await vessel_object_type.add_object(ns_idx, "State", objecttype=vessel_state_object_type) # <--------------------- wird nicht richtig instanziert!
-    await vessel_object_state.set_modelling_rule(True)
+
+    # Metadaten
+    vessel_meta_object = await vessel_object_type.add_object(ns_idx, "Meta")
+    await vessel_meta_object.add_reference(ua.ObjectIds.ModellingRule_Mandatory, ua.ObjectIds.HasModellingRule, True, False)
+
+    vessel_meta_name = await vessel_meta_object.add_property(ns_idx, "Name", "", ua.VariantType.String)
+    await vessel_meta_name.set_modelling_rule(True)
+
+    vessel_meta_location= await vessel_meta_object.add_property(ns_idx, "Location", "", ua.VariantType.String)
+    await vessel_meta_location.set_modelling_rule(True)
+
+    vessel_meta_serial = await vessel_meta_object.add_property(ns_idx, "SerialNumber", "", ua.VariantType.String)
+    await vessel_meta_serial.set_modelling_rule(True)
+
+    vessel_meta_volume = await vessel_meta_object.add_property(ns_idx, "Volume", "", ua.VariantType.String)
+    await vessel_meta_volume.set_modelling_rule(True)
 
 
-    # '''
-    # Behälter:
-    # '''
-    # vessel_object_type = await base_object_type.add_object_type(ns_idx, "VesselObjectType")
+    # Behälter State
+    vessel_state_object = await vessel_object_type.add_object(ns_idx, "State")
+    await vessel_state_object.add_reference(ua.ObjectIds.ModellingRule_Mandatory, ua.ObjectIds.HasModellingRule, True, False)
 
-    # #mandatory:
-    # vessel_name = await vessel_object_type.add_variable(ns_idx, "Name", "BehälterName", ua.VariantType.String)
-    # await vessel_name.set_writable(False) # True = writable
-    # await vessel_name.set_modelling_rule(True) # True = mandatory
+    vessel_state_operationalstate = await vessel_state_object.add_variable(ns_idx, "OperationalState", 0, ua.VariantType.Int64) # Datentyp?
+    await vessel_state_operationalstate.set_modelling_rule(True) # Mandatory ?
 
-    # vessel_location = await vessel_object_type.add_variable(ns_idx, "Location", "BehälterLocation", ua.VariantType.String)
-    # await vessel_location.set_writable(False) # True = writable
-    # await vessel_location.set_modelling_rule(True) # True = mandatory
+    vessel_state_operationalhours = await vessel_state_object.add_variable(ns_idx, "OperationalHours", 0, ua.VariantType.Int64) # Datentyp?
+    await vessel_state_operationalhours.set_modelling_rule(True) # Mandatory ?
 
-    # vessel_state = await vessel_object_type.add_variable(ns_idx, "State", 0, ua.VariantType.Int16)
-    # await vessel_state.set_writable(False) # True = writable
-    # await vessel_state.set_modelling_rule(True) # True = mandatory
+    vessel_state_poweronhours = await vessel_state_object.add_variable(ns_idx, "PowerOnHours", 0, ua.VariantType.Int64) # Datentyp? -> hh:mm:ss ?
+    await vessel_state_poweronhours.set_modelling_rule(True) # Mandatory ?
 
-    # vessel_level = await vessel_object_type.add_variable(ns_idx, "Level", 0.0, ua.VariantType.Float)
-    # await vessel_level.set_writable(False) # True = writable
-    # await vessel_level.set_modelling_rule(True) # True = mandatory
+    # Prozessdaten
+    vessel_process_daten_object = await vessel_object_type.add_object(ns_idx, "Process")
+    await vessel_process_daten_object.add_reference(ua.ObjectIds.ModellingRule_Mandatory, ua.ObjectIds.HasModellingRule, True, False)
 
-    # #optional:
-    # # ... WIP!
-    
-    
-    # '''
-    # Rührwerk:
-    # '''
-    # agitator_object_type = await base_object_type.add_object_type(ns_idx, "AgitatorObjectType")
+    vessel_process_speed = await vessel_process_daten_object.add_object(ns_idx, "AgitatorSpeed")
+    await vessel_process_speed.add_reference(ua.ObjectIds.ModellingRule_Mandatory, ua.ObjectIds.HasModellingRule, True, False)
+    vessel_speed_setpoint = await vessel_process_speed.add_variable(ns_idx, "SetpointValue", 0, ua.VariantType.Float)
+    await vessel_speed_setpoint.set_modelling_rule(True) # Mandatory ?
+    vessel_speed_act_val = await vessel_process_speed.add_variable(ns_idx, "ActualValue", 0, ua.VariantType.Float)
+    await vessel_speed_act_val.set_modelling_rule(True) # Mandatory ? NEIN zum testen JA
 
-    # #mandatory:
-    # agitator_name = await agitator_object_type.add_variable(ns_idx, "Name", "RührwerkName", ua.VariantType.String)
-    # await agitator_name.set_writable(False) # True = writable
-    # await agitator_name.set_modelling_rule(True) # True = mandatory
+    vessel_process_temperature = await vessel_process_daten_object.add_object(ns_idx, "Temperature")
+    await vessel_process_temperature.add_reference(ua.ObjectIds.ModellingRule_Mandatory, ua.ObjectIds.HasModellingRule, True, False)
+    vessel_process_temperature_setpoint = await vessel_process_temperature.add_variable(ns_idx, "SetpointValue", 0, ua.VariantType.Float)
+    await vessel_process_temperature_setpoint.set_modelling_rule(True) # Mandatory ?
+    vessel_process_temperature_act_val = await vessel_process_temperature.add_variable(ns_idx, "ActualValue", 0, ua.VariantType.Float)
+    await vessel_process_temperature_act_val.set_modelling_rule(True) # Mandatory ? NEIN zum testen JA
 
-    # agitator_location = await agitator_object_type.add_variable(ns_idx, "Location", "RührwerkLocation", ua.VariantType.String)
-    # await agitator_location.set_writable(False) # True = writable
-    # await agitator_location.set_modelling_rule(True) # True = mandatory
+    vessel_process_level = await vessel_process_daten_object.add_object(ns_idx, "Level")
+    await vessel_process_level.add_reference(ua.ObjectIds.ModellingRule_Mandatory, ua.ObjectIds.HasModellingRule, True, False)
 
-    # agitator_speed = await agitator_object_type.add_variable(ns_idx, "Speed", 0.0, ua.VariantType.Float)
-    # await agitator_speed.set_writable(False) # True = writable
-    # await agitator_speed.set_modelling_rule(True) # True = mandatory
+    vessel_process_level_volume = await vessel_process_level.add_object(ns_idx, "Volume")
+    await vessel_process_level_volume.add_reference(ua.ObjectIds.ModellingRule_Mandatory, ua.ObjectIds.HasModellingRule, True, False)
+    vessel_process_level_volume_measured = await vessel_process_level_volume.add_variable(ns_idx, "Measured", 0, ua.VariantType.Float)
+    await vessel_process_level_volume_measured.set_modelling_rule(True) # Mandatory ?
+    vessel_process_level_volume_calculated = await vessel_process_level_volume.add_variable(ns_idx, "Calculated", 0, ua.VariantType.Float)
+    await vessel_process_level_volume_calculated.set_modelling_rule(True) # Mandatory ?
 
-    
-    # #optional:
-    # # ... WIP!
-    
-    # '''
-    # Ventil:
-    # '''
-    # valve_object_type = await base_object_type.add_object_type(ns_idx, "ValveObjectType")
-
-    # #mandatory:
-    # valve_name = await valve_object_type.add_variable(ns_idx, "Name", "VentilName", ua.VariantType.String)
-    # await valve_name.set_writable(False) # True = writable
-    # await valve_name.set_modelling_rule(True) # True = mandatory
-
-    # valve_type = await valve_object_type.add_variable(ns_idx, "Type", "VentilTyp", ua.VariantType.String)
-    # await valve_type.set_writable(False) # True = writable
-    # await valve_type.set_modelling_rule(True) # True = mandatory
-
-    # valve_state = await valve_object_type.add_variable(ns_idx, "State", 0, ua.VariantType.Int16)
-    # await valve_state.set_writable(False) # True = writable
-    # await valve_state.set_modelling_rule(True) # True = mandatory
-
-    # #optional:
-    # valve_hub = await valve_object_type.add_variable(ns_idx, "Hub", 0.0, ua.VariantType.Float)
+    vessel_process_level_weight = await vessel_process_level.add_object(ns_idx, "Weight")
+    await vessel_process_level_weight.add_reference(ua.ObjectIds.ModellingRule_Mandatory, ua.ObjectIds.HasModellingRule, True, False)
+    vessel_process_level_weight_measured = await vessel_process_level_weight.add_variable(ns_idx, "Measured", 0, ua.VariantType.Float)
+    await vessel_process_level_weight_measured.set_modelling_rule(True) # Mandatory ?
+    vessel_process_level_weight_calculated = await vessel_process_level_weight.add_variable(ns_idx, "Calculated", 0, ua.VariantType.Float)
+    await vessel_process_level_weight_calculated.set_modelling_rule(True) # Mandatory ?
 
     # Export Namespace as xml
     try:
