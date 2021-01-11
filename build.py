d89bc54e912f3428c8ccab17f7f34097cdc7e139
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2021-01-11 16:14:21 +0100

    fix(Server): update missing references

--- build.py
+++ build.py
@@ -1,15 +1,18 @@
 import os, datetime
 
-with open("buildlog.txt", "a+") as f:
+with open("log/buildlog.txt", "a+") as f:
     f.write(f"Build: {datetime.datetime.now()} from {os.getlogin()}\n")
     try:
-       os.system("python src/generate_export_namespace.py")
-       f.write("Namespace build passed!\n")
-    except Exception as e:
-        f.write(f"{e}\n")
-    try:
-       os.system("pyinstaller --onefile src/server.py")
-       f.write("Executable build passed!\n")
+        p = os.system("python src/generate_export_namespace.py")
+        if str(p) == "1":
+            f.write("Namespace build failed!\n")
+        else:
+            f.write("Namespace build passed!\n")
+            p = os.system("pyinstaller --onefile src/server.py")
+            if str(p) == "1":
+                f.write("Executable build failed!\n")
+            else:
+                f.write("Executable build passed!\n")
     except Exception as e:
         f.write(f"{e}\n")
     f.write("-------------")
