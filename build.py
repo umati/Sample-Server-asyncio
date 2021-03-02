7c8e1ba9509d628ea39d149cfd9d7ffba1f0326e
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2021-03-02 15:36:50 +0100

    fix(server): update server and nodesets

--- build.py
+++ build.py
@@ -3,16 +3,11 @@ import os, datetime
 with open("log/buildlog.txt", "a+") as f:
     f.write(f"Build: {datetime.datetime.now()} from {os.getlogin()}\n")
     try:
-        p = os.system("python src/generate_export_namespace.py")
+        p = os.system("pyinstaller --onefile src/server.py")
         if str(p) == "1":
-            f.write("Namespace build failed!\n")
+            f.write("Executable build failed!\n")
         else:
-            f.write("Namespace build passed!\n")
-            p = os.system("pyinstaller --onefile src/server.py")
-            if str(p) == "1":
-                f.write("Executable build failed!\n")
-            else:
-                f.write("Executable build passed!\n")
+            f.write("Executable build passed!\n")
     except Exception as e:
         f.write(f"{e}\n")
     f.write("-------------")
