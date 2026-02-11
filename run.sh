2f20babfe3c16ddd456e891fd5a4b1802adcbe0a
Goetz Goerisch <g.goerisch@vdw.de>
2026-02-11 15:40:02 +0100

    fix: align workflows
    Signed-off-by: Goetz Goerisch <g.goerisch@vdw.de>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright 2021 (c) Moritz Walker, ISW University of Stuttagart (for umati and VDW e.V.)
 
-docker build -t opcuaserver .
+docker build -t opcuaserver . --no-cache
 docker container rm -f opcuaserver
-docker run --name=opcuaserver -p 4840:4840 opcuaserver
+docker run --name=opcuaserver --network=host opcuaserver
